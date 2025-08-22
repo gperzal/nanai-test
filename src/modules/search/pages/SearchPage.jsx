069cf53eeb825a5751cc95bb666c1b1c1cb2ba8d
Guido Perez <gperzal@gmail.com>
2025-08-21 23:22:16 -0400

    jajaja2

--- src/modules/search/pages/SearchPage.jsx
+++ src/modules/search/pages/SearchPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import './../styles/SearchPage.css';
+import './SearchPage.css';
 
 
 const SearchPage = () => {
--- src/modules/test/pages/TestPage.jsx
+++ src/modules/test/pages/TestPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './../styles/TestPage.css';
+import '../../../assets/css/TestPage.css';
 
 const TestPage = () => {
   const navigate = useNavigate();
